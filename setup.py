65515b2b4d472a37ad5b29bcb043ad2392987665
Kevin Yap <me@kevinyap.ca>
2014-06-29 14:53:09 -0700

    Added Python 3.2 and 3.3 classifiers to package

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
     ],
     keywords=['EulerPy', 'euler', 'project-euler', 'projecteuler'],
     include_package_data=True,
