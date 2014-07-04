f72750943656a72454b01fcfe4343d74b6102e4a
Kevin Yap <me@kevinyap.ca>
2014-07-03 18:15:20 -0700

    Improved test coverage
    test_program_flow() tests the package as if it were being run from the
    command line using CliRunner.

--- EulerPy/tests.py
+++ EulerPy/tests.py
@@ -1,13 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import os
 import unittest
 import textwrap
+
+from click.testing import CliRunner
 
 from EulerPy import euler
 
 class Tests(unittest.TestCase):
     def setUp(self):
         pass
+
+
+    def test_program_flow(self):
+        """Check that EulerPy is executing properly"""
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            # Test "N" as file generation prompt input
+            result = runner.invoke(euler.main, input='N\n')
+            self.assertEqual(result.exit_code, 1)
+            self.assertFalse(os.path.isfile('001.py'))
+
+            # Test "Y" as file generation prompt input
+            result = runner.invoke(euler.main, input='\n')
+            self.assertEqual(result.exit_code, None)
+            self.assertTrue(os.path.isfile('001.py'))
+
+            # 001.py has been generated, so attempt to verify solution
+            result = runner.invoke(euler.main)
+            self.assertEqual(result.exit_code, 1)
 
 
     def test_problem_format(self):
@@ -44,7 +66,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(euler.get_filename(10), "010.py")
         self.assertEqual(euler.get_filename(100), "100.py")
 
-        self.assertRaises(ValueError, euler.get_filename, "string")
+        self.assertRaises(ValueError, euler.get_filename, "foo")
 
 
 if __name__ == '__main__':
